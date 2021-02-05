9ea093c3dc124546cc822384ed4915d18607aca0
Gwyneth Yuen <gwynethyuen@users.noreply.github.com>
2021-02-04 22:17:39 -0500

    First commit

--- discussion/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.discussion_index, name="discussion_index"),
-    path("<int:pk>/", views.discussion_detail, name="discussion_detail"),
-    path("<category>/", views.discussion_category, name="discussion_category"),
-]
